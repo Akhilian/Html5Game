4a817de6b5a8ec8763bd75c8a2b2017357af0194
Alwin <contact@alwin.fr>
2013-09-09 20:41:37 +0200

    Comment on CONFIG

--- js/CONFIG.js
+++ js/CONFIG.js
@@ -1,2 +1,13 @@
+
+	// Image per second
 var FRAME_RATE = 40,
-	READY_TO_DRAW;
+	
+	// Speed limit for characters
+	MAX_SPEED = 30,
+
+	// Canvas to be drawn
+	CANVAS = $("#map"),
+
+	// Context
+	CONTEXT = CANVAS[0].getContext('2d');
+
