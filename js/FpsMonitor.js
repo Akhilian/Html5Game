dcc3ae77c686fdae1ce8dfc627ce56cc2c4c9488
Alwin <contact@alwin.fr>
2013-09-14 18:35:21 +0200

    Adding a FPS monitor.
    Set DISPLAY_FPS_MONITOR to true in the config file to display an FPS
    monitor. Allow to measure performance.

---
+++ js/FpsMonitor.js
@@ -0,0 +1,33 @@
+var FpsMonitor = {
+
+	// Set FPS Monitor environment
+	init : function() {
+
+		this.fpsMonitor = new Stats();
+		this.fpsMonitor.setMode(0);
+
+		this.fpsMonitor.domElement.style.position = 'absolute';
+		this.fpsMonitor.domElement.style.left = '0px';
+		this.fpsMonitor.domElement.style.top = '0px';
+
+		document.body.appendChild( this.fpsMonitor.domElement );
+
+	},
+
+	begin : function() {
+
+		if( DISPLAY_FPS_MONITOR)
+		{
+			if( this.fpsMonitor != undefined )
+				this.fpsMonitor.begin();
+			else
+				throw "Please set up the monitor by calling FpsMonitor.init() first.";
+		}
+	},
+
+	end : function() {
+		this.fpsMonitor.end();
+	}
+
+
+}
