d2544a5278b686f8bef8917cbfaea65863bdc7d2
Alwin <contact@alwin.fr>
2013-09-14 18:31:29 +0200

    New config file. Set up global value.

--- js/CONFIG.js
+++ js/CONFIG.js
@@ -5,9 +5,20 @@ var FRAME_RATE = 40,
 	// Speed limit for characters
 	MAX_SPEED = 30,
 
+	SPEED_X = 10,
+
 	// Canvas to be drawn
 	CANVAS = $("#map"),
 
 	// Context
-	CONTEXT = CANVAS[0].getContext('2d');
+	CONTEXT = CANVAS[0].getContext('2d'),
+
+	// Tile size
+	TILE_SIZE = 70,
+
+	// Gravity
+	GRAVITY_UP = 0.70,
+	GRAVITY_DOWN = 1.1,
+
+	DISPLAY_FPS_MONITOR = true;
 
