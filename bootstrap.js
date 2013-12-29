4ef67019aead0ae657dadb3a8bd98cf568741679
Alwin <contact@alwin.fr>
2013-12-30 00:55:11 +0100

    Configuration for PhysicsJS + Stats

--- bootstrap.js
+++ bootstrap.js
@@ -2,5 +2,24 @@ require.config({
 
 	baseUrl: "js/",
 
+	paths: {
+		"jquery" : '../tools/jquery',
+		"Stats" : '../tools/Stats'
+	},
+
+	packages: [
+		{
+			name: 'physicsjs',
+			location: '../tools/physicsjs',
+			main: 'physicsjs-0.5.3.min'	
+		}
+	],
+
+	shim: {
+		'Stats': {
+			exports: 'Stats'
+		}
+	},
+
 	deps: ["start"]
 });
