3de2cfaaa2991016682dce05a993f99630d4b01f
Alwin <contact@alwin.fr>
2014-01-31 15:47:53 +0100

    Build and testing automation with Grunt.js

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+
+	// Project configuration
+	grunt.initConfig({
+
+		pkg: grunt.file.readJSON('package.json'),
+
+		clean: {
+			build: {
+				src: ["build/release/*"]
+			},
+			qa: {
+				src: ["build/report/*"]
+			}
+		},
+
+		jshint: {
+			files: ['./*.js', 'js/*']
+		},
+
+		watch: {
+			files: ['<%= jshint.files %>'],
+			tasks: ['jshint']
+		}
+
+	});
+
+	// Tasks list
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	//Default task
+	grunt.registerTask('default', ['clean:qa', 'jshint']);
+};
