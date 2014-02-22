bca4d6445561076b44873caaf2a11469078a8aa0
Alwin <contact@alwin.fr>
2014-02-22 23:12:30 +0100

    Reset of Tile object

--- js/view/Tile.js
+++ js/view/Tile.js
@@ -1,39 +1,8 @@
-define(['CONFIG'], function(CONFIG) {
+define(['CONFIG', 'maps/Position'], function(CONFIG, Position) {
 
-	function Tile(img) {
-	
-		// Attributs
-		this.imgSrc = img;
-		this.tileIsLoaded = false;
+	function Tile(position) {
+		this.position = position;
 	}
-
-	Tile.prototype.drawAt = function(x, y, convert) {
-
-		var height = CONFIG.CANVAS.height(),
-			width = CONFIG.CANVAS.width();
-
-		if(convert === undefined){
-				
-			CONFIG.CONTEXT.drawImage(
-				this.imgSrc,
-				CONFIG.TILE_SIZE * x,
-				height - CONFIG.TILE_SIZE * ( y )
-			);
-
-		}
-		else
-		{
-			CONFIG.CONTEXT.drawImage(
-				this.imgSrc,
-				x,
-				height - y - ( this.imgSrc.height )
-			);
-		}		
-	};
-
-	Tile.prototype.tileLoaded = function() {
-		this.tileIsLoaded = true;
-	};
 
 	return Tile;
 
